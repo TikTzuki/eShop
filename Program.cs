7b75c0bafd5e6caa8b0598ae2668f4e2363f4375
TikTzuki <tranphanthanhlong18@gmail.com>
2021-03-02 18:48:02 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eeShop.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace eeShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().SeedDatabase().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
+    public static class IHostExtenions{
+        public static IHost SeedDatabase(this IHost host){
+             var scopeFactory = host.Services.GetRequiredService<IServiceScopeFactory>();
+             
+            using var scope = scopeFactory.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<ProductContext>();
+
+            if (context.Database.EnsureCreated())
+                SeedData.Initialize(context);
+
+            return host;
+        }
+    }
+}
